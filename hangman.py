8e7eace3b828ab9319e44ce25376d3e179f8dca7
achen6159 <31477651+achen6159@users.noreply.github.com>
2017-11-14 08:47:39 -0500

    added basic features

--- hangman.py
+++ hangman.py
@@ -1,8 +1,13 @@
-def start_screen():
+#Hangman Game
+#
+#Annie C.
+
+
+def show_start_screen():
     print("Welcome to the hangman game!")
 
 def show_credits():
-
+    pass
 def get_puzzle():
     return "hangman"
 
@@ -18,14 +23,24 @@ def get_solved(puzzle, guesses):
     return solved
 
 def get_guess():
-    letter = input("Guess a letter:")
-    return letter
+    while True:
+        letter = input("Guess a letter: ")
 
-def display_board(solved):
+        if str(letter) == 1:
+            return letter
+        else:
+            print("I don't understand. Enter a letter please.")
+    
+        
+def display_board(solved, strikes):
     print(solved)
-
-def show_result():
-    print("You win!")
+    print("Strikes: " + str(strikes))
+    
+def show_result(strikes):
+    if strikes < limit:
+        print("You winn!!!!")
+    else:
+        print("Your not good enough")
 
 def play_again():
     while True:
@@ -38,17 +53,27 @@ def play_again():
         else:
             print("I don't understand. Please enter 'y' or 'n'.")
     
-def play():
+def play(strikes, limit):
     puzzle = get_puzzle()
-    guess = ""
+    guesses = ""
     solved = get_solved(puzzle, guesses)
-    display_board(solved)
+    display_board(solved, strikes)
+
+    print(solved)
 
     while solved != puzzle:
-        guesses += get_guess()
-        solved = get_solved(puzzle, guesses)
-        display_board(solved)
+        letter = get_guess()
 
+        if letter not in puzzle:
+            strikes += 1
+            if strikes == limit:
+                break
+            
+        guesses += letter
+        solved = get_solved(puzzle, guesses)
+        display_board(solved, strikes)
+
+    show_result(strikes)
     
 #Game starts running here
 show_start_screen()
@@ -56,8 +81,11 @@ show_start_screen()
 playing = True
 
 while playing:
-    play()
-    show_result()
+    strikes = 0
+    limit = 6
+
+    print("You have " + str(limit) + " tries")
+    play(strikes, limit)
     playing = play_again()
 
-show_credits
+show_credits()
